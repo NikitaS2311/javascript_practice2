f5ec9fd5272dc83e2b5b96fc78e3f5aa83201d1b
NikitaS2311 <96657334+NikitaS2311@users.noreply.github.com>
2024-02-25 09:57:40 +0000

    container config done

---
+++ basics/loop.js
@@ -0,0 +1,15 @@
+
+let obj= {
+    harry: 33,
+    shiv: 34,
+    ritika: 36,
+    shivalik: 78
+}
+//for in loop
+for(let a in obj){
+    console.log("Marks of " +   a +" are" +obj[a])
+}
+// For of loop
+for(let b of "Nikita"){
+    console.log(b)
+}
